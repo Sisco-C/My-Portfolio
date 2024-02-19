aad51581dc9d92dcbaf089094a73faf4fa7c3a56
Sisco-C <siscoronojey@gmail.com>
2024-02-19 23:35:29 +0300

    Resume update

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/Sisco-C/My-Portfolio/master/src/Assets/Sisco_Cherono_resume.pdf";
+  "https://raw.githubusercontent.com/Sisco-C/My-Portfolio/master/src/Assets/Sisco_Cherono_Resumes.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
